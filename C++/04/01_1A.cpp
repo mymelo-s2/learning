ecde97125f89c9a6cbf42e38ba202b45291f3da0
Uran Mikami <122005359+mymelo-s2@users.noreply.github.com>
2024-03-04 10:00:39 +0900

    Create 01_1A.cpp

---
+++ C++/04/01_1A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    string Str = "paiza";
+    for(int i = 0; i < Str.size(); i++) {
+        cout << Str[i];
+    }
+    cout << endl;
+    return 0;
+}
