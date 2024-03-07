030300a150d000c54667bd0b0df8ae59521c9f76
Uran Mikami <122005359+mymelo-s2@users.noreply.github.com>
2024-03-07 11:37:05 +0900

    Add files via upload

---
+++ C++/06/02/WordCount.cpp
@@ -0,0 +1,21 @@
+/*
+ * PaizaQuestion.cpp
+ *
+ *  Created on: 2024/03/07
+ */
+#include "../header/WordCount.hpp"
+
+int WordCount::get()
+{
+    return wordLen;
+}
+
+void WordCount::set( int const value )
+{
+	wordLen = value;
+}
+
+
+
+
+
---
+++ C++/06/02/WordCount.hpp
@@ -0,0 +1,15 @@
+/*
+ * PaizaQuestion.h
+ *
+ *  Created on: 2024/03/07
+ */
+#pragma once
+
+class WordCount {
+  private:
+    int wordLen;
+
+  public:
+	int get();
+	void set(int value);
+};
