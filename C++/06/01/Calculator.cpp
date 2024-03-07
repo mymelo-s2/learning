f7c11c4824ddf96eee72b73be2039fcab5eb508e
Uran Mikami <122005359+mymelo-s2@users.noreply.github.com>
2024-03-07 10:53:10 +0900

    Add files via upload

---
+++ C++/06/01/Calculator.cpp
@@ -0,0 +1,23 @@
+/*
+ * Calculator.cpp
+ *
+ *  Created on: 2024/03/07
+ */
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch(oper)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            return 0.0;
+    }
+}
---
+++ C++/06/01/Calculator.h
@@ -0,0 +1,13 @@
+/*
+ * Calcilator.h
+ *
+ *  Created on: 2024/03/07
+ */
+#pragma once
+class Calculator {
+  public:
+    double Calculate(double x, char oper, double y);
+};
+
+
+
