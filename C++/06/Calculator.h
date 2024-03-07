8b4498d32d5e871d32485b871296449d74d8cf59
Uran Mikami <122005359+mymelo-s2@users.noreply.github.com>
2024-03-07 10:50:20 +0900

    Add files via upload

---
+++ C++/06/Calculator.h
@@ -0,0 +1,14 @@
+/*
+ * Calcilator.h
+ *
+ *  Created on: 2024/03/07
+ *      Author: u-mikami
+ */
+#pragma once
+class Calculator {
+  public:
+    double Calculate(double x, char oper, double y);
+};
+
+
+
